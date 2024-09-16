df4ad3639721f5bfcf4500dd7cea75b35013e228
elkopfoffice <93712781+elkopfoffice@users.noreply.github.com>
2024-09-16 21:04:16 +0000

    changes

--- Ex_Files/02_02_begin/main.py
+++ Ex_Files/02_02_begin/main.py
@@ -10,3 +10,7 @@ greet_format = "Hello {}"
 formatted = greet_format.format(name)
 
 print(intrupution, formatted)
+
+print(formatted.upper()+' '+formatted.lower())
+
+print(formatted.replace("John","Paul"))
